4ea0e4fc3cbbf10f0fd7c076c85072945ca66e98
Sanjit Shrestha <forofficialsanjit@gmail.com>
2019-10-19 15:19:25 +0545

    initial commit

---
+++ src/main/java/com/hireoeasy/starter/HireOEasyApplication.java
@@ -0,0 +1,13 @@
+package com.hireoeasy.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HireOEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HireOEasyApplication.class, args);
+	}
+
+}
