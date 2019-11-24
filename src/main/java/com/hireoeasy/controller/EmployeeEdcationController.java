b4bf381a3b445fa3c02ddfc3372c5b9a9c1543c7
kryshna <erkmpbhw@gmail.com>
2019-11-24 21:36:49 +0545

    minor error

--- src/main/java/com/hireoeasy/controller/EmployeeEdcationController.java
+++ src/main/java/com/hireoeasy/controller/EmployeeEdcationController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,8 @@ import com.hireoeasy.service.EmployeeEducationService;
 import com.hireoeasy.service.UserDetailService;
 
 @Controller
+@CrossOrigin(origins = "*")
+
 public class EmployeeEdcationController {
 	
 	@Autowired
