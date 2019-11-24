98cca73dd1ab545376d7d4c0d2974bf259070af5
kryshna <erkmpbhw@gmail.com>
2019-11-24 22:05:16 +0545

    return of user education fix #1

--- src/main/java/com/hireoeasy/controller/EmployeeEdcationController.java
+++ src/main/java/com/hireoeasy/controller/EmployeeEdcationController.java
@@ -25,55 +25,68 @@ import com.hireoeasy.service.UserDetailService;
 @CrossOrigin(origins = "*")
 
 public class EmployeeEdcationController {
-	
+
 	@Autowired
 	private EmployeeEducationService educationService;
 	@Autowired
 	private UserDetailService userDetailService;
-	
+
 	// Get education
-		@GetMapping(value = "/webapi/employee/employeedetail/education/{user_detail_id}")
-		public ResponseEntity<List<EmployeeEducation>> getAllJobs(@PathVariable("user_detail_id") Long id) {
-			Optional<UserDetail> userDetail = userDetailService.findByid(id);
-			List<EmployeeEducation> educationList = new ArrayList<>();
-			if(userDetail.isPresent()) {
-				educationList = educationService.findEducationByUserDetailId(id);
-			}
-			return ResponseEntity.ok().body(educationList);
-		}
+	// @GetMapping(value =
+	// "/webapi/employee/employeedetail/education/{user_detail_id}")
+	// public ResponseEntity<List<EmployeeEducation>>
+	// getAllJobs(@PathVariable("user_detail_id") Long id) {
+	// Optional<UserDetail> userDetail = userDetailService.findByid(id);
+	// List<EmployeeEducation> educationList = new ArrayList<>();
+	// if(userDetail.isPresent()) {
+	//
+	// educationList = educationService.findEducationByUserDetailId(id);
+	// }
+	// return ResponseEntity.ok().body(educationList);
+	// }
 
-		// save new education 
-		@PostMapping(value = "/webapi/employee/employeedetail/education/{id}/save")
-		public ResponseEntity<?> postEducation(@RequestBody EmployeeEducation employeeEducation,@PathVariable("id") Long id) {
-			Optional<UserDetail> userDetail = userDetailService.findByid(id);
-			UserDetail userDetailData;
-			if(userDetail.isPresent()) {
-				userDetailData = userDetail.get();
-			} else {
-				userDetailData = null;
-			}
-			employeeEducation.setUserDetail(userDetailData);
-			educationService.save(employeeEducation);
-			return ResponseEntity.ok().body("Employee education Success.");
+	@GetMapping(value = "/webapi/employee/employeedetail/education/{user_detail_id}")
+	public ResponseEntity<List<EmployeeEducation>> getAllJobs(@PathVariable("user_detail_id") Long id) {
+		Optional<UserDetail> userDetail = userDetailService.findByid(id);
+		List<EmployeeEducation> educationList = new ArrayList<>();
+		if (userDetail.isPresent()) {
+			educationList = userDetail.get().getEmployeeEducation();
 		}
+		return ResponseEntity.ok().body(educationList);
+	}
 
-		// Delete education
-		@DeleteMapping(value = "/webapi/employee/employeedetail/education/delete/{id}")
-		public ResponseEntity<?> deleteEducation(@PathVariable("id") Long id) {
-			educationService.delete(id);
-			return ResponseEntity.ok().body("education Deleted");
+	// save new education
+	@PostMapping(value = "/webapi/employee/employeedetail/education/{id}/save")
+	public ResponseEntity<?> postEducation(@RequestBody EmployeeEducation employeeEducation,
+			@PathVariable("id") Long id) {
+		Optional<UserDetail> userDetail = userDetailService.findByid(id);
+		UserDetail userDetailData;
+		if (userDetail.isPresent()) {
+			userDetailData = userDetail.get();
+		} else {
+			userDetailData = null;
 		}
+		employeeEducation.setUserDetail(userDetailData);
+		educationService.save(employeeEducation);
+		return ResponseEntity.ok().body("Employee education Success.");
+	}
 
-		// Get a single education for edit and show by Id
-		@GetMapping(value = "/webapi/employee/employeedetail/education/show/{id}")
-		public ResponseEntity<EmployeeEducation> getJob(@PathVariable("id") Long id) {
-			Optional<EmployeeEducation> education = educationService.findByid(id);
-			if (education.isPresent()) {
-				EmployeeEducation employeeEducationData = education.get();
-				return ResponseEntity.ok().body(employeeEducationData);
-			}
-			return ResponseEntity.ok().body(null);
+	// Delete education
+	@DeleteMapping(value = "/webapi/employee/employeedetail/education/delete/{id}")
+	public ResponseEntity<?> deleteEducation(@PathVariable("id") Long id) {
+		educationService.delete(id);
+		return ResponseEntity.ok().body("education Deleted");
+	}
+
+	// Get a single education for edit and show by Id
+	@GetMapping(value = "/webapi/employee/employeedetail/education/show/{id}")
+	public ResponseEntity<EmployeeEducation> getJob(@PathVariable("id") Long id) {
+		Optional<EmployeeEducation> education = educationService.findByid(id);
+		if (education.isPresent()) {
+			EmployeeEducation employeeEducationData = education.get();
+			return ResponseEntity.ok().body(employeeEducationData);
 		}
-
+		return ResponseEntity.ok().body(null);
+	}
 
 }
