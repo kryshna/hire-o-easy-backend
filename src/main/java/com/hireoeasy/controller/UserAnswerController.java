1d83ac117ef0d26a5f6d6560c7a4e1335e64374d
kryshna <erkmpbhw@gmail.com>
2019-11-25 09:18:37 +0545

    userAnswer post success

---
+++ src/main/java/com/hireoeasy/controller/UserAnswerController.java
@@ -0,0 +1,53 @@
+package com.hireoeasy.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.hireoeasy.domain.EmployeeEducation;
+import com.hireoeasy.domain.Exam;
+import com.hireoeasy.domain.UserAnswer;
+import com.hireoeasy.domain.UserDetail;
+import com.hireoeasy.service.ExamService;
+import com.hireoeasy.service.UserAnswerService;
+import com.hireoeasy.service.UserDetailService;
+
+@Controller
+public class UserAnswerController {
+
+	@Autowired
+	private UserAnswerService answerService;
+	@Autowired
+	private ExamService examService;
+	@Autowired
+	private UserDetailService userDetailService;
+
+	// save answer
+	@PostMapping(value = "/webapi/employee/{employee_id}/jobapply/exam/{exam_id}/submitanswer")
+	public ResponseEntity<?> postEducation(@RequestBody UserAnswer answer,
+			@PathVariable("employee_id") Long employeeDetailId, @PathVariable("exam_id") Long id) {
+		Optional<Exam> examDetail = examService.findById(id);
+		Optional<UserDetail> userDetail = userDetailService.findByid(employeeDetailId);
+		Exam examData;
+		UserDetail userDetaildata;
+		if (examDetail.isPresent() && userDetail.isPresent()) {
+			examData = examDetail.get();
+			userDetaildata = userDetail.get();
+
+		} else {
+			examData = null;
+			userDetaildata = null;
+
+		}
+		answer.setUserId(userDetaildata);
+		answer.setExampId(examData);
+		answerService.save(answer);
+		return ResponseEntity.ok().body("anser added Success.");
+	}
+
+}
--- src/main/java/com/hireoeasy/service/UserAnswerService.java
+++ src/main/java/com/hireoeasy/service/UserAnswerService.java
@@ -3,6 +3,7 @@ package com.hireoeasy.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.hireoeasy.domain.UserAnswer;
 import com.hireoeasy.repository.UserAnswerRepository;
 
 @Service
@@ -10,5 +11,10 @@ public class UserAnswerService {
 	
 	@Autowired
 	private UserAnswerRepository answerRepository;
+	
+	public void save(UserAnswer answer) {
+		
+		answerRepository.save(answer);
+	}
 
 }
